f550e356cb4dfba7fe43499a25ff2bd053487930
Prem386 <premkadam0506@gmail.com>
2026-07-15 14:46:57 +0530

    coordinate picker location

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import IndiaMap from "./components/IndiaMap/IndiaMap";
-import { clientLocations } from "./data/clientLocations";
+import CoordinatePicker from "./components/CoordinatePicker/CoordinatePicker";
 
 function App() {
-  // ponytail: render the presentational map component with client locations data
   return (
     <div className="app">
-      <IndiaMap clientLocations={clientLocations} />
+      <CoordinatePicker />
     </div>
   );
 }
